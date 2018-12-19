e8cb53fedea26ba25d1b68dc8a44d1145aaee25b
Thomas Lau <thomas_0836@qq.com>
2018-12-19 10:07:18 +0800

    init

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const url = require('./lib/url');
 const where = require('./lib/where');
 
+console.log('url', url)
+console.log('where', where)
+
 module.exports = {
   url,
   where
--- test/index.js
+++ test/index.js
@@ -1,3 +1,44 @@
-'use strict'
+const assert = require('power-assert');
+const util = require('../')
+const { url, where } = util;
 
-require('./url');
+describe('Url test', () => {  
+  it('getPageQuery test', () => {    
+    const { getPageQuery } = url
+    const testUrl = 'http://baidu.com?a=1&b=2';
+    const query = getPageQuery(testUrl);
+    assert(query);
+    assert(query.a === '1');
+    assert(query.b === '2');
+  });
+
+  it('addQuery test', () => {    
+    const { addQuery } = url
+    const testUrl = 'http://baidu.com?a=1&b=2';
+    const thisUrl = addQuery(testUrl, 'lan', 'en');
+    assert(thisUrl);
+    assert(thisUrl.indexOf('lan=en') > 0);
+    assert(thisUrl.indexOf('a=1') > 0);
+    assert(thisUrl.indexOf('b=2') > 0);
+    assert(thisUrl.indexOf('baidu.com') > 0);
+    assert(thisUrl === 'http://baidu.com?a=1&b=2&lan=en');
+
+  });
+});
+
+describe('where test', () => {  
+  it('should work', () => {    
+    window = { navigator: { userAgent: 'MicroMessenger'}}
+    let wh = where()
+    assert(wh === 'wechat');
+
+    window = { navigator: { userAgent: 'phone'}}
+    wh = where()
+    assert(wh === 'mobile');
+
+    window = { navigator: { userAgent: 'pc'}} 
+    wh = where()
+    assert(wh === 'pc');
+  });
+});
+
