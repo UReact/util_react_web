eeb9dcffc4e8a75ea46acfbcb089fe7eb3d59fb9
Thomas Lau <thomas_0836@qq.com>
2018-12-21 11:42:20 +0800

    add i18n util

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const url = require('./lib/url');
 const where = require('./lib/where');
 const delay = require('./lib/delay');
+const string = require('./lib/string');
 
 module.exports = {
   url,
   where,
-  delay
+  delay,
+  string
 }
